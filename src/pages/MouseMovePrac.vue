44399d0e94efb0b6461ea4732e7eb3092026ea06
leedohyeon0502 <leedohyeon0502@gmail.com>
2022-08-19 02:45:38 +0900

    newyorktimes allbirds

--- src/pages/MouseMovePrac.vue
+++ src/pages/MouseMovePrac.vue
@@ -1,7 +1,13 @@
 <template>
-    <!-- <h1>test</h1> -->
-    <div class="cursor_item"></div>
-    <button type="button">START</button>
+    <img src="https://paidpost-assets.nyt.com/paidpost/allbirds/birds-eye-view/images/1-b90e91a4.png">
+
+<img src="https://paidpost-assets.nyt.com/paidpost/allbirds/birds-eye-view/images/2-f3fd6cf5.png">
+
+<div class="vod"><video autoplay="" playsinline="" loop="" muted="" src="https://paidpost-assets.nyt.com/paidpost/allbirds/birds-eye-view/videos/3-27c8c3ea.webm"></video></div>
+
+<img src="https://paidpost-assets.nyt.com/paidpost/allbirds/birds-eye-view/images/4-105dd65b.png">
+
+<img src="https://paidpost-assets.nyt.com/paidpost/allbirds/birds-eye-view/images/5-ac49bfdc.png">
 </template>
 
 <script>
@@ -11,80 +17,47 @@
 
     let x = 0;
     let y = 0;
-    // let mx = 0;
-    // let my = 0;
-    // let speed = 0.08;
-
-    let cursor_item;
-    // let h1;
+    let mx = 0;
+    let my = 0;
+    let speed = 0.09;
+    let imgArray;
+    let bird;
 
     window.onload = function(){
-        //h1 = document.getElementsByTagName("h1")[0];
-        cursor_item = document.getElementsByClassName("cursor_item")[0];
-        window.addEventListener("click", mouseFunc, false);
+        window.addEventListener("mousemove", mouseFunc, false);
+        imgArray = document.getElementsByTagName("img");
+        bird = document.getElementsByClassName("vod")[0];
 
         function mouseFunc(e){
-            x = e.clientX;
-            y = e.clientY;
-            cursor_item.style.transform = "translate(" + x + "px, " + y + "px)";
-            
+            x = (e.clientX - window.innerWidth / 2);
+            y = (e.clientY - window.innerHeight / 2);
         }
-        //loop();
+        loop();
     }
 
-    // function loop(){
-    //     //console.log(x);
-    //     mx += (x - mx) * speed;
-    //     my += (y - my) * speed;
-    //     //console.log(x, mx)
+    function loop(){
+        mx += (x - mx) * speed;
+        my += (y - my) * speed;
 
-    //     h1.innerHTML = "x: " + mx + " y: " + my;
+        console.log(x, y);
 
-    //     cursor_item.style.transform = "translate(" + mx + "px, " + my + "px)";
-    //     window.requestAnimationFrame(loop);
-    // }
-            //cursor.log(e.clientX, e.clientY);
+        for(let i = 0; i < imgArray.length; i++){
+            imgArray[i].style.transform = "translate(" + -(mx/(16-i*2)) + "px, " + -(my/(10-i)) + "px)";
+        }
+
+        bird.style.transform = "translate(" + -(mx/20) + "px, " + -(my/20) + "px)";
+
+        window.requestAnimationFrame(loop);
+    }
 
 </script>
 
 <style>
-    body{
-        position: relative;
-        background-color: black;
-
-        margin: 0;
+    img{
+        position: absolute;
     }
 
-    h1{
-        color: #fff;
-    }
-
-    button{
-        font-size : 40px;
-        font-weight: bold;
-        background: #fff;
-        color: red;
-        padding: 30px 80px;
-        margin: 50px;
-        cursor: pointer;
-        transition: all 500ms cubic-bezier(0.755, 0.010, 0.310, 1.000);
-    }
-
-    button:hover{
-        background: red;
-        color: #fff;
-        font-size: 80px;
-        padding: 80px 150px;
-    }
-
-    .cursor_item{
-        position: fixed;
-        width: 100px;
-        height: 100px;
-        background-color: red;
-        top: 0;
-        left: 0;
-        margin: -50px 0 0 -50px;
-        transition: all 500ms cubic-bezier(0.755, 0.010, 0.310, 1.000);
+    .vod{
+        position: absolute;
     }
 </style>
