6a554e97451e660df6c501274065dd8284871186
leedohyeon0502 <leedohyeon0502@gmail.com>
2022-08-19 01:35:43 +0900

    transition, easing (가속도) realize

--- src/pages/MouseMovePrac.vue
+++ src/pages/MouseMovePrac.vue
@@ -1,6 +1,7 @@
 <template>
-    <h1>test</h1>
+    <!-- <h1>test</h1> -->
     <div class="cursor_item"></div>
+    <button type="button">START</button>
 </template>
 
 <script>
@@ -10,36 +11,38 @@
 
     let x = 0;
     let y = 0;
-    let mx = 0;
-    let my = 0;
-    let speed = 0.08;
+    // let mx = 0;
+    // let my = 0;
+    // let speed = 0.08;
 
     let cursor_item;
-    let h1;
+    // let h1;
 
     window.onload = function(){
-        h1 = document.getElementsByTagName("h1")[0];
+        //h1 = document.getElementsByTagName("h1")[0];
         cursor_item = document.getElementsByClassName("cursor_item")[0];
-        window.addEventListener("mousemove", mouseFunc, false);
+        window.addEventListener("click", mouseFunc, false);
 
         function mouseFunc(e){
             x = e.clientX;
             y = e.clientY;
-            //cursor.log(e.clientX, e.clientY);
+            cursor_item.style.transform = "translate(" + x + "px, " + y + "px)";
+            
         }
-        loop();
+        //loop();
     }
 
-    function loop(){
-        //console.log(x);
-        mx += (x - mx) * speed;
-        my += (y - my) * speed;
-        //console.log(x, mx)
+    // function loop(){
+    //     //console.log(x);
+    //     mx += (x - mx) * speed;
+    //     my += (y - my) * speed;
+    //     //console.log(x, mx)
 
-        h1.innerHTML = "x: " + mx + " y: " + my;
-        cursor_item.style.transform = "translate(" + mx + "px, " + my + "px)";
-        window.requestAnimationFrame(loop);
-    }
+    //     h1.innerHTML = "x: " + mx + " y: " + my;
+
+    //     cursor_item.style.transform = "translate(" + mx + "px, " + my + "px)";
+    //     window.requestAnimationFrame(loop);
+    // }
 
 </script>
 
@@ -47,18 +50,40 @@
     body{
         position: relative;
         background-color: black;
+
+        margin: 0;
     }
 
     h1{
         color: #fff;
     }
 
+    button{
+        font-size : 40px;
+        font-weight: bold;
+        background: #fff;
+        color: red;
+        padding: 30px 80px;
+        margin: 50px;
+        cursor: pointer;
+        transition: all 500ms cubic-bezier(0.755, 0.010, 0.310, 1.000);
+    }
+
+    button:hover{
+        background: red;
+        color: #fff;
+        font-size: 80px;
+        padding: 80px 150px;
+    }
+
     .cursor_item{
-        position: absolute;
+        position: fixed;
         width: 100px;
         height: 100px;
         background-color: red;
         top: 0;
         left: 0;
+        margin: -50px 0 0 -50px;
+        transition: all 500ms cubic-bezier(0.755, 0.010, 0.310, 1.000);
     }
 </style>
