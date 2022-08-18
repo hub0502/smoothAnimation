bc11828fc5d92a3a53f57f639ab857e886aa1f25
leedohyeon0502 <leedohyeon0502@gmail.com>
2022-08-19 00:36:34 +0900

    window addEvent(mouseEvent)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import Home from './pages/HomePage.vue'
+import MouseMovePrac from './pages/MouseMovePrac.vue'
+
+const routes = [
+    {name : 'home', path : '/', component : Home},
+    {name : 'museMove', path : '/mouseMove', component : MouseMovePrac}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes, 
+  })
+  
+  const app = createApp(App)
+  app.use(router)
+  
+  app.mount('#app')
---
+++ src/pages/MouseMovePrac.vue
@@ -0,0 +1,43 @@
+<template>
+    <h1>test</h1>
+    <div class="cursor_item"></div>
+</template>
+
+<script>
+export default {
+    
+}
+
+window.onload = function(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let cursor_item = document.getElementsByClassName("cursor_item")[0];
+    window.addEventListener("mousemove", mouseFunc, false);
+
+    function mouseFunc(e){
+        h1.innerHTML = "x: " + e.clientX + " y: " + e.clientY;
+        cursor_item.style.transform = "translate(" + e.clientX + "px, " + e.clientY + "px)";
+        //curser.log(e.clientX, e.clientY);
+    }
+}
+
+</script>
+
+<style>
+    body{
+        position: relative;
+        background-color: black;
+    }
+
+    h1{
+        color: #fff;
+    }
+
+    .cursor_item{
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        background-color: red;
+        top: 0;
+        left: 0;
+    }
+</style>
